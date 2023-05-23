ceb56deba0561f3e75255a155f91157b83f88354
puckipsi <angelskijm@gmail.com>
2023-05-23 22:36:52 +0300

    add chatbot service

---
+++ chatbot/ChatBot.py
@@ -0,0 +1,45 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import HumanMessage
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+
+from utils.config import config
+
+
+class ChatBot:
+
+
+    def __init__(self, vectors: list):
+        self.openai_api_key = config.OPENAI_API_KEY
+        self.model_name = config.OPENAI_MODEL
+        self.temperature = config.OPENAI_TEMPERATURE
+        self.vectors = vectors
+
+    template = """
+        You are a helpful AI assistant named Nifty Bridge AI assistant. The user gives you a file, and its content is represented by the following pieces of context. I will use this context to answer the question at the end.
+        If you don't know the answer, just say "I don't know please contact with support by email support@nifty-bridge.com". Do NOT try to make up an answer.
+        If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to Nifty Bridge.
+        context: {context}
+        question: {question}
+        """
+
+    PROMPT = PromptTemplate(template=template, input_variables=["context", "question"])
+
+    def interact_with_user(self, question: str):
+        print(self.openai_api_key, self.model_name, self.temperature)
+        llm = ChatOpenAI(openai_api_key=self.openai_api_key, model_name=self.model_name, temperature=self.temperature)
+
+        retriever = self.vectors.as_retriever()
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=retriever,
+            verbose=True,
+            return_source_documents=True,
+            combine_docs_chain_kwargs={"prompt": self.PROMPT},
+        )
+
+        chain_input = {"question": question}
+        result = chain(chain_input)
+    
+        return result["answer"]
